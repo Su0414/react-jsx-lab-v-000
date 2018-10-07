e858c2c980fe6aeaafb196f88fa4434a2b82b409
Suchitra Naik <susanaik@gmail.com>
2018-10-07 20:43:31 +0000

    automatically backed up by learn

---
+++ src/components/Webpage.js
@@ -0,0 +1,12 @@
+class Webpage extends Component {
+  render() {
+    return (
+        <title>
+          I am a filler text. I can be used to fill your screen. Amazing!
+        </title>
+        <FilterText />
+        <FilterText />
+
+    )
+  }
+}
