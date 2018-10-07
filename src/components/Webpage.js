dedd9b0f5e3e91ce3635f4e49ed6dbaf4822d12d
Suchitra Naik <susanaik@gmail.com>
2018-10-07 21:02:35 +0000

    automatically backed up by learn

--- src/components/Webpage.js
+++ src/components/Webpage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import FilterText from './components/FilterText';
+
 export default class Webpage extends React.Component {
   render() {
     return (
-
+      
         <div>
         <h1>The worlds coolest webpage</h1>
         <FilterText />
