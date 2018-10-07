3c8b30d3bdcf5c36fc6debd1faa4595e9a452ce8
Suchitra Naik <susanaik@gmail.com>
2018-10-07 20:45:12 +0000

    automatically backed up by learn

--- src/components/Webpage.js
+++ src/components/Webpage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-class Webpage extends React.Component {
+export default class Webpage extends React.Component {
   render() {
     return (
         <title>The worlds coolest webpage</title>
