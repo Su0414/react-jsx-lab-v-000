732b49226d5cacd4839c9cf2ded0d560c2c84e90
Suchitra Naik <susanaik@gmail.com>
2018-10-07 20:38:05 +0000

    automatically backed up by learn

---
+++ src/components/RegistrationForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class RegistrationForm extends React.Component {
+  render() {
+    return (
+            <div className="user_form">
+              <form>
+                <input type="text" name="username"/>
+                <input type="password" name="password"/>
+                <button type="submit"></button>
+              </form>
+            </div>
+    )
+  }
+}
